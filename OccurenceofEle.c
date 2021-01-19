20710803ffce9876a38d1239c4f96cc93c5af958
KundrapuSivani <77660053+KundrapuSivani@users.noreply.github.com>
2021-01-19 13:07:22 +0530

    Create OccurenceofEle.c
    Convert an unsorted array into sorted array then find the starting and ending position of a given value in that sorted array

---
+++ OccurenceofEle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main()
+{
+int i, j,num, temp, n, a[20];
+printf("Enter the value of n \n");
+scanf("%d", &n);
+printf("Enter the numbers \n");
+for(i = 0;i < n;++i)
+scanf("%d", &a[i]);
+for(i =0;i< n;++i)
+{
+for(j =i+1;j< n;++j)
+{
+if(a[i]>a[j])
+{
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+}
+}
+}
+printf("The numbers arranged in ascending order are given below \n");
+for (i = 0; i < n; ++i)
+printf("%d\n", a[i]);
+printf("Enter a element");
+scanf("%d",&num);
+for(i=0;i<n;++i)
+{
+if(a[i]==num)
+{
+printf("The occurence of number %d is at %d\n",num,i);
+}
+}
+}
