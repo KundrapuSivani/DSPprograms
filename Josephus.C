79c7a87ec93f5650054331e78e978113319371c6
KundrapuSivani <77660053+KundrapuSivani@users.noreply.github.com>
2021-01-20 17:41:56 +0530

    Update Josephus.C

--- Josephus.C
+++ Josephus.C
@@ -2,81 +2,81 @@
 #include <stdlib.h>
 struct node
 {
-int number;
-struct node *next;
+    int number;
+    struct node *next;
 };
 void create(struct node **);
 void display(struct node *);
 int survivor(struct node **, int);
 int main()
 {
-struct node *head = NULL;
-int survive, skip;
-create(&head);
-printf("The persons in circular list are:\n");
-display(head);
-printf("Enter the number of persons to be skipped: ");
-scanf("%d", &skip);
-survive = survivor(&head, skip);
- printf("The person to survive is : %d\n", survive);
-free(head);
-return 0;
+     struct node *head = NULL;
+     int survive, skip;
+     create(&head);
+     printf("The persons in circular list are:\n");
+     display(head);
+     printf("Enter the number of persons to be skipped: ");
+     scanf("%d", &skip);
+     survive = survivor(&head, skip);
+     printf("The person to survive is : %d\n", survive);
+     free(head);
+     return 0;
 }
 int survivor(struct node **head, int k)
 {
-struct node *p, *q;
- int i;
- q = p = *head;
-while (p->next != p)
-{
-for (i = 0; i < k - 1; i++)
- {
-q = p;
-p = p->next;
- }
-q->next = p->next;
-printf("%d has been killed.\n", p->number);
-free(p);
-p = q->next;
-}
-*head = p;
-return (p->number);
+    struct node *p, *q;
+    int i;
+    q = p = *head;
+    while (p->next != p)
+    {
+        for (i = 0; i < k - 1; i++)
+        {
+            q = p;
+            p = p->next;
+        }
+       q->next = p->next;
+       printf("%d has been killed.\n", p->number);
+       free(p);
+       p = q->next;
+    }
+   *head = p;
+   return (p->number);
 }
 void create (struct node **head)
 {
-struct node *temp, *rear;
-int a, ch;
-do
-{
-printf("Enter a number: ");
-scanf("%d", &a);
-temp = (struct node *)malloc(sizeof(struct node));
-temp->number = a;
-temp->next = NULL;
-if (*head == NULL)
-{
-*head = temp;
-}
- else
-{
-rear->next = temp;
-}
-rear = temp;
-printf("Do you want to add a number [1/0]? ");
-scanf("%d", &ch);
-} while (ch != 0);
-rear->next = *head;
+    struct node *temp, *rear;
+    int a, ch;
+    do
+    {
+         printf("Enter a number: ");
+         scanf("%d", &a);
+         temp = (struct node *)malloc(sizeof(struct node));
+         temp->number = a;
+         temp->next = NULL;
+         if (*head == NULL)
+         {
+            *head = temp;
+         }
+         else
+         {
+            rear->next = temp;
+         }
+         rear = temp;
+         printf("Do you want to add a number [1/0]? ");
+         scanf("%d", &ch);
+    } while (ch != 0);
+    rear->next = *head;
 }
 void display(struct node *head)
 {
-struct node *temp;
-temp = head;
- printf("%d   ", temp->number);
-temp = temp->next;
-while (head != temp)
-{
- printf("%d   ", temp->number);
-temp = temp->next;
- }
-printf("\n");
+    struct node *temp;
+    temp = head;
+    printf("%d ", temp->number);
+    temp = temp->next;
+    while (head != temp)
+    {
+        printf("%d", temp->number);
+        temp = temp->next;
+    }
+    printf("\n");
 }
