658dbe85bb7b87c84bae7f80bd58852d15fe23a9
KundrapuSivani <77660053+KundrapuSivani@users.noreply.github.com>
2021-01-20 17:01:06 +0530

    Update QueueLL.c

--- QueueLL.c
+++ QueueLL.c
@@ -3,65 +3,65 @@
 #include<stdlib.h>
 struct node
 {
-int data;
-struct node *next;
+    int data;
+    struct node *next;
 }*front = NULL,*rear = NULL;
 int enque(int value)
 {
-struct node *newnode;
-newnode = (struct node*)malloc(sizeof(struct node));
-newnode->data = value;
-newnode -> next = NULL;
-if(front == NULL)
-{
-front = newnode;
-rear = newnode;
-}
-else
-{
-rear -> next = newnode;
-rear = newnode;
-}
+    struct node *newnode;
+    newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = value;
+     newnode -> next = NULL;
+     if(front == NULL)
+     {
+        front = newnode;
+        rear = newnode;
+     }
+     else
+     {
+         rear -> next = newnode;
+         rear = newnode;
+     }
 }
 void deque()
 {
-if(front == NULL)
-printf("\nQ underflow");
-else
-{
-struct node *temp = front;
-front = front -> next;
-printf("\nDeleted element: %d\n", temp->data);
-free(temp);
-}
+    if(front == NULL)
+    printf("\nQ underflow");
+    else
+    {
+        struct node *temp = front;
+        front = front -> next;
+        printf("\nDeleted element: %d\n", temp->data);
+        free(temp);
+    }
 }
 void display()
 {
-if(front == NULL)
-printf("\nQ is underflow");
-else
-{
-struct node *temp = front;
-while(temp->next != NULL)
-{
-printf("\n%d element in Q",temp->data);
-temp = temp -> next;
-printf("\n%d element in Q",temp->data);
-}
-}
+     if(front == NULL)
+     printf("\nQ is underflow");
+     else
+     {
+         struct node *temp = front;
+         while(temp->next != NULL)
+         {
+             printf("\n%d element in Q",temp->data);
+             temp = temp -> next;
+             printf("\n%d element in Q",temp->data);
+         }
+     }
 }
 int main()
 {
-int i,n,a;
-printf("enter the number of elements in the Q ");
-scanf("%d",&n);
-printf("enter %d elemnts into the Q\n",n);
-for(i=0;i<n;i++)
-{
-scanf("%d",&a);
-enque(a);
-}
-display();
-deque();
-return 0;
+     int i,n,a;
+     printf("enter the number of elements in the Q ");
+     scanf("%d",&n);
+     printf("enter %d elemnts into the Q\n",n);
+     for(i=0;i<n;i++)
+     {
+         scanf("%d",&a);
+         enque(a);
+     }
+     display();
+     deque();
+     return 0;
 }
