24bb517fbb380d2fc685c727a30cce532b93869c
KundrapuSivani <77660053+KundrapuSivani@users.noreply.github.com>
2021-02-13 00:26:51 +0530

    Update BreadthFirstSearch.C

--- BreadthFirstSearch.C
+++ BreadthFirstSearch.C
@@ -1,61 +1,61 @@
 #include<stdio.h>
 #include<conio.h>
-int adj[30][30],que[30],visited[30];
-int front=0,rear=0,m,n;
-void BFStrav(int m);
+#define MAX 30
+int adj[MAX][MAX],queue[MAX],visited[MAX];
+int front=0,rear=0,vertex,n;
+void BFStrav(int vertex);
 void main()
 {
-   int i,j,c=0;
-   printf("\n enter number of nodes in the graph:");
-   scanf( "%d",&n);
-   printf("\n enter the adjacency matrix\n");
-   for(i=1;i<=n;i++)
-   {
-      for(j=1;j<=n;j++)
+      int i,j,count=0;
+      printf("\n Enter number of nodes in the graph:");
+      scanf( "%d",&n);
+      printf("\n Enter the adjacency matrix\n");
+      for(i=1;i<=n;i++)
       {
-         scanf("%d",&adj[i][j]);
+            for(j=1;j<=n;j++)
+            {
+                scanf("%d",&adj[i][j]);
+            }
       }
-   }
-  for(i=1;i<=n;i++)
-  {
-     visited[i]=0;
-     que[i]=0;
-  }
-  printf("\n enter the starting node of the graph");
-  scanf("%d",&m);
-  BFStrav(m);
-  for(i=1; i <= n; i++)
-  {
-     if(visited[i])
-     c++;
-  }
-  if(c==n)
-  {
-     printf("\n all nodes are reachable from starting vertex using BFS :\n");
-     for(i=1;i<=n;i++)
-     {
-        if(visited[i])
-        printf("%d \t",i);
-     }
-   }
-  else 
-  {
-     printf("\n all nodes are not reachable and BFS is not possible");
-   }
+      for(i=1;i<=n;i++)
+      {
+           visited[i]=0;
+           queue[i]=0;
+      }
+      printf("\n Enter the starting vertex of the graph :");
+      scanf("%d",&vertex);
+      BFStrav(vertex);
+      for(i=1; i <= n; i++)
+      {
+           if(visited[i])
+           count++;
+      }
+      if(count==n)
+      {
+          printf("\n All nodes are reachable from starting vertex using BFS\n");
+          for(i=1;i<=n;i++)
+          {
+                if(visited[i])
+                    printf("%d \t",i);
+          }
+      }
+      else
+          printf("\n All nodes are not reachable ");
+      
 }
-void BFStrav(int m) 
+void BFStrav(int vertex) 
 {
-   int i,k;
-   for(i= 1;i<= n;i++)
-   if(adj[m][i]&&!visited[i])
-   {
-      que[rear] = i;
-      rear++;
-    }
-    if(front<=rear) 
-    {
-      k=que[front];    
-      visited[k] = 1;
-      BFStrav(que[front++]);
-    }
+      int i,j;
+      for(i= 1;i<= n;i++)
+           if(adj[vertex][i]&&!visited[i])
+           {
+               queue[rear] = i;
+               rear++;
+           }
+      if(front<=rear) 
+      {
+           j=queue[front];
+           visited[j] = 1;
+           BFStrav(queue[front++]);
+      }
 }
