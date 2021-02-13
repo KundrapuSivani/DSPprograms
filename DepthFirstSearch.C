172e99ffb451ed20c644a2c1eb42ab28153e6504
KundrapuSivani <77660053+KundrapuSivani@users.noreply.github.com>
2021-02-13 11:31:47 +0530

    Update DepthFirstSearch.C

--- DepthFirstSearch.C
+++ DepthFirstSearch.C
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 30
-int v,n,adj[MAX][MAX],visited[MAX];
-void DFStrav(int v);
+int v,n,visited[MAX];
 int stack[MAX];
 int top = -1;
 void push(int v)
@@ -31,28 +30,48 @@ int pop()
               return v;
         }
 }
- 
-int isEmptyStack( )
+ void DFS(int a[n][n],int v)
 {
-      if(top==-1)
-             return 1;
-      else
-           return 0;
+    int flag=0;
+    push(v);
+    while(top!=-1 )
+    {
+        v=pop();
+
+        if(visited[v]==0)
+        {
+           visited[v]=1;
+           printf("node %d is visited\n",v);
+
+           for(int i=0;i<n;i++)
+          {
+
+              if(a[i][v]==1||a[v][i]==1)
+
+                  push(i);
+          }
+
+        }
+     }
 }
+
+
  
 int main()
 {
    int i,j;
+    int a[9][9] = {   {0,1,0,1,0,0,0,0,1},
+                      {1,0,0,0,0,0,0,1,0},
+                      {0,0,0,1,0,1,0,1,0},
+                      {1,0,1,0,1,0,0,0,0},
+                      {0,0,0,1,0,0,0,0,1},
+                      {0,0,1,0,0,0,1,0,0},
+                      {0,0,0,0,0,1,0,0,0},
+                      {0,1,1,0,0,0,0,0,0},
+                      {1,0,0,0,1,0,0,0,0}}; 
    printf("\n Enter number of nodes in the graph :");
    scanf( "%d",&n);
-   printf("\n Enter the adjacency matrix:\n");
-   for(i=0;i<n;i++)
-   {
-      for(j=0;j<n;j++)
-      {
-          scanf("%d",&adj[i][j]);
-      }
-   }
+  
    for(i=0;i<n;i++)
    {
       visited[i]=0;
@@ -60,25 +79,8 @@ int main()
    printf("\n Enter the starting node of the graph :");
    scanf("%d",&v);
    printf("\nDFS traversal for the given graph:");
-   DFStrav(v);  
+   DFS(a,v);  
    return 0;
 }
-void DFStrav(int v)
-{
-        int i;
-        push(v);
-        while(!isEmptyStack())
-        {
-                v = pop();
-                if(!visited[v])
-                {
-                      printf("%d ",v);
-                      visited[v]=1;
-                }
-                for(i=0;i<n;i++)
-                {
-                      if(adj[v][i]==1 && !visited[i])
-                             push(i);
-                }
-        }
-}
+
+
